ebb8441dfc4c45ff7b14246617c165439f3a1e7e
Your Name <dwijjoshi02@gmail.com>
2025-05-03 01:35:18 +0530

    Bills

--- server/app.js
+++ server/app.js
@@ -20,9 +20,9 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v1", bills);
 app.use("/api/v1", user);
 app.use("/api/v1", transaction);
+app.use("/api/v1", bills);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 module.exports = app;
