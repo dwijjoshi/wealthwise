4764dbe502a9c3f9ed26fc79eb4ec99afe0c3b09
Your Name <dwijjoshi02@gmail.com>
2025-05-03 02:47:08 +0530

    DATAbase url

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 exports.connectDatabase = () => {
   mongoose
     .connect(
-      "mongodb+srv://dwijjoshi02:1kg3aSSBZCz63xo1@wealthwise.gao49wg.mongodb.net/"
+      "mongodb+srv://aasthatandel93:rYYAqrGBISDzi5SF@wealthwise.kjqnrni.mongodb.net/"
     )
     .then((con) => console.log(`Database connected : ${con.connection.host}`))
     .catch((err) => console.log(err));
